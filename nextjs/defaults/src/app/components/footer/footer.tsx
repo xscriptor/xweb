3518ea5c3bd1ebc087374053359098facfc4f958
xscriptorcode <preciado.oscar.osorio@gmail.com>
2025-09-29 13:04:51 +0200

    add footer in defaults

---
+++ nextjs/defaults/src/app/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+//footer default template
+
+export default function Footer () {
+    return(
+        <div className="flex justify-center text-center items-center p-2">
+            <p>X©</p>
+        </div>
+    )
+}
+
+// Use: <Footer /> and import as you need
